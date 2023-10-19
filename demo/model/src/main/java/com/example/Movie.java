ffed3cb3e00130c34c584e7ee62ce69e1c994c02
cori oyarzabal <corina.oyarzabal@xcaleconsulting.com>
2023-10-19 20:50:18 -0300

    modules

---
+++ demo/model/src/main/java/com/example/Movie.java
@@ -0,0 +1,55 @@
+package com.example;
+
+
+public class Movie {
+
+
+    private Integer id;
+    private String title;
+    private String director;
+    private String genre;
+
+    public Movie() {
+
+    }
+
+    public Movie(Integer id, String title, String director, String genre) {
+
+        this.id = id;
+        this.title = title;
+        this.director = director;
+        this.genre = genre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
---
+++ demo/model/src/main/java/com/example/MovieTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class MovieTest {
+
+    private Movie movie = new Movie();
+    public void testGetId() {
+
+        movie.setId(1);
+       // assertNotNull(movie.getId());
+    }
+}
